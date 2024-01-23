877b7af26e4c300ced9c3ca93b55ebdafd189964
Kamil Grząba <kamil.grzaba@gmail.com>
2024-01-23 14:23:11 +0100

    basic package cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
 import * as PiwikPro from '../core'
 export * as PageViews from '../services/pageViews/pageViews.service'
 
-createApp(App).mount('#app')
 export default {
   initialize: PiwikPro.default.init,
 }
