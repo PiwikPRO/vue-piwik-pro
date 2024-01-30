3908460b1bff6789a98f677d76d828de0cca512b
Kamil Grząba <kamil.grzaba@gmail.com>
2024-01-30 19:36:42 +0100

    example - daisyui package added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-essential',
     'prettier',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   overrides: [
