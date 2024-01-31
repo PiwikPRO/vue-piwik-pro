e936e84141582a35bcbc0fd1a02b3560fb4d49c6
Kamil Grząba <kamil.grzaba@gmail.com>
2024-01-31 10:12:05 +0100

    example - adds router

--- vue-piwik-pro-example/tailwind.config.cjs
+++ vue-piwik-pro-example/tailwind.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   content: ['./src/**/*.{vue,js,ts}'],
-  plugins: [require('daisyui'), require("@tailwindcss/typography")],
+  plugins: [require('daisyui'), require('@tailwindcss/typography')],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 }
