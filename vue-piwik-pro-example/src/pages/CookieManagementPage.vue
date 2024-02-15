e3e42c4a4032ea854c9de6cac8a803fbda1fa044
Kamil Grząba <kamil.grzaba@gmail.com>
2024-02-15 09:41:19 +0100

    method name instead of method method

--- vue-piwik-pro-example/src/pages/CookieManagementPage.vue
+++ vue-piwik-pro-example/src/pages/CookieManagementPage.vue
@@ -10,97 +10,97 @@ const pageData = {
     'Collection of methods to manage cookies through the Piwik PRO API.',
   methods: [
     {
-      method: 'enableCookies',
+      name: 'enableCookies',
       usage: 'CookieManagement.enableCookies();',
       desc: 'Enables all first party cookies. Cookies will be created on the next tracking request.',
       args: null,
     },
     {
-      method: 'disableCookies',
+      name: 'disableCookies',
       usage: 'CookieManagement.disableCookies();',
       desc: 'Disables all first party cookies. Existing cookies will be deleted in the next page view.',
       args: null,
     },
     {
-      method: 'deleteCookies',
+      name: 'deleteCookies',
       usage: 'CookieManagement.deleteCookies();',
       desc: 'Deletes existing tracking cookies on the next page view.',
       args: null,
     },
     {
-      method: 'hasCookies',
+      name: 'hasCookies',
       usage: 'CookieManagement.hasCookies();',
       desc: 'Returns true if cookies are enabled in this browser.',
       args: null,
     },
     {
-      method: 'setCookieNamePrefix',
+      name: 'setCookieNamePrefix',
       usage: 'CookieManagement.setCookieNamePrefix("_examplePrefix_");',
       desc: 'Sets the prefix for analytics tracking cookies. Default is "_pk_".',
       args: '_examplePrefix_',
     },
     {
-      method: 'setCookieDomain',
+      name: 'setCookieDomain',
       usage: 'CookieManagement.setCookieDomain(".example.com");',
       desc: 'Sets the domain for the analytics tracking cookies.',
       args: '.example.com',
     },
     {
-      method: 'getCookieDomain',
+      name: 'getCookieDomain',
       usage: 'CookieManagement.getCookieDomain();',
       desc: 'Returns domain of the analytics tracking cookies (set with setCookieDomain()).',
       args: null,
     },
     {
-      method: 'setCookiePath',
+      name: 'setCookiePath',
       usage: 'CookieManagement.setCookiePath("/blog/");',
       desc: 'Sets the analytics tracking cookies path.',
       args: '/blog/',
     },
     {
-      method: 'getCookiePath',
+      name: 'getCookiePath',
       usage: 'CookieManagement.getCookiePath();',
       desc: 'Returns the analytics tracking cookies path.',
       args: null,
     },
     {
-      method: 'setSecureCookie',
+      name: 'setSecureCookie',
       usage: 'CookieManagement.setSecureCookie(true);',
       desc: 'Toggles the secure cookie flag on all first party cookies (if you are using HTTPS).',
       args: true,
     },
     {
-      method: 'setVisitorCookieTimeout',
+      name: 'setVisitorCookieTimeout',
       usage: 'CookieManagement.setVisitorCookieTimeout(33955200);',
       desc: 'Sets the expiration time of visitor cookies.',
       args: 33955200,
     },
     {
-      method: 'getConfigVisitorCookieTimeout',
+      name: 'getConfigVisitorCookieTimeout',
       usage: 'CookieManagement.getConfigVisitorCookieTimeout();',
       desc: 'Returns expiration time of visitor cookies (in milliseconds).',
       args: null,
     },
     {
-      method: 'setReferralCookieTimeout',
+      name: 'setReferralCookieTimeout',
       usage: 'CookieManagement.setReferralCookieTimeout(15768000);',
       desc: 'Sets the expiration time of referral cookies.',
       args: 15768000,
     },
     {
-      method: 'setSessionCookieTimeout',
+      name: 'setSessionCookieTimeout',
       usage: 'CookieManagement.setSessionCookieTimeout(1800000);',
       desc: 'Sets the expiration time of session cookies.',
       args: 1800000,
     },
     {
-      method: 'getSessionCookieTimeout',
+      name: 'getSessionCookieTimeout',
       usage: 'CookieManagement.getSessionCookieTimeout();',
       desc: 'Returns expiration time of session cookies.',
       args: null,
     },
     {
-      method: 'setVisitorIdCookie',
+      name: 'setVisitorIdCookie',
       usage: 'CookieManagement.setVisitorIdCookie();',
       desc: 'Sets cookie containing analytics ID in browser.',
       args: null,
@@ -134,8 +134,8 @@ const showToast = (message: string) => {
         </div>
         <h2>Methods</h2>
         <ul>
-          <li v-for="method in pageData.methods" :key="method.method">
-            <h4>{{ method.method }}</h4>
+          <li v-for="method in pageData.methods" :key="method.name">
+            <h4>{{ method.name }}</h4>
             <p>{{ method.desc }}</p>
             <highlightjs language="typescript" :code="method.usage" />
             <button
@@ -143,11 +143,11 @@ const showToast = (message: string) => {
               @click="
                 () => {
                   CookieManagement[
-                    method.method as keyof typeof CookieManagement
+                    method.name as keyof typeof CookieManagement
                     // @ts-ignore-next-line
                   ](method.args as any)
                   showToast(
-                    `CookieManagement.${method.method}(${method.args ?? ''})`
+                    `CookieManagement.${method.name}(${method.args ?? ''})`
                   )
                 }
               "
