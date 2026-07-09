c69379914336ee306925644e442a6eeefe4b790c
Pawel Fajfer <p.fajfer@piwik.pro>
2026-07-09 13:29:36 +0200

    chore: bump tracking-base-library from 1.7.0 to 1.7.1 (#61)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as PiwikPRO from '@piwikpro/tracking-base-library'
+import PiwikPro from '@piwikpro/tracking-base-library'
 export * from '@piwikpro/tracking-base-library'
 import { VERSION } from './version'
 import { Initialize, Miscellaneous } from '@piwikpro/tracking-base-library'
@@ -8,11 +8,10 @@ const initialize: Initialize = (...args) => {
     Miscellaneous.setTrackingSourceProvider('vue', VERSION)
   }
 
-  PiwikPRO.default.initialize(...args)
+  PiwikPro.initialize(...args)
 }
 
 export default {
-  ...PiwikPRO.default,
   initialize,
-  // fixes some 'The inferred type of 'default' cannot be named without a reference to ...' error
-} as typeof PiwikPRO.default
+  getInitScript: PiwikPro.getInitScript,
+}
